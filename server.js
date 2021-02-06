506b2c384d541f64b7db479788e6963b7d9e7f26
Tomás Catena <tomascatena@gmail.com>
2021-02-06 17:52:24 +0100

    CRUD functionality for Bootcamps added

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const color = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === 'development') {
@@ -20,8 +28,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} monde on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} monde on port ${PORT}`.yellow
+      .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`.red.bold);
+  // Close server & exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
